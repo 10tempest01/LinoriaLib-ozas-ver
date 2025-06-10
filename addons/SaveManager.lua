119aa77bdfc09241854213ebb54ba7320811aad8
Tempest <59133811+10tempest01@users.noreply.github.com>
2025-06-10 20:11:33 +0300

    Update SaveManager.lua

--- addons/SaveManager.lua
+++ addons/SaveManager.lua
@@ -276,8 +276,8 @@ local SaveManager = {} do
                 xpcall(function()
                     task.wait(2.5)
                     if not self.Library or not self.Library.RegistryMap then return; end
-                    for i, v in pairs(ContainerLabel) do
-                        if v.KeybindLabel and v.Properties.TextColor3 ~= "AccentColor" then
+                    for i, v in pairs(self.Library.RegistryMap[ContainerLabel]) do
+                        if v.KEYBINDLABEL and v.Properties.TextColor3 ~= "AccentColor" then
                             v.Visible = not Toggles.OnlyShowEnabledKeybinds.Value;
                         end;
                     end;
